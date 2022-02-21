763195e062ae7410b4828564dacf402b574a6635
Cfnatic7 <michalpasicki1@gmail.com>
2022-02-21 05:48:29 +0100

    The app seems to be ready for deployment

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,4 +1,3 @@
-import logo from '../../logo.svg';
 import './App.css';
 import Header from '../header/Header';
 import Main from '../main/Main'
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,8 +1,11 @@
 import './Header.css'
 
 const Header = () => {
-    return (<header className = 'position-fixed w-100'>
-                <h2>Quote machine</h2>
+    return (<header className = 'position-fixed w-100 title'>
+                <div className = 'd-flex justify-content-between'>
+                    <h2 >Quote machine</h2>
+                    <h6>by <a href = 'https://github.com/Cfnatic7' className = 'github-link'>Cfnatic7</a></h6>
+                </div>
             </header>);
 }
 
