ae60840d1a7ce5a34dd5e3a4f96a6726a2669391
Cfnatic7 <michalpasicki1@gmail.com>
2022-02-21 01:10:49 +0100

    Rewritten header to make it stateless

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,28 +1,9 @@
 import './Header.css'
-import {Component} from 'react';
 
-export default class Header extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            icon: 'moon'
-        }
-        this.handleClick = this.handleClick.bind(this);
-    }
-    handleClick() {
-        this.setState( state => {
-            if (state.icon === 'moon') return {
-                icon: 'sun'
-            };
-            else return {
-                icon: 'moon'
-            };
-        })
-    }
-    render() {
-        return (<header className = 'container-fluid Success'>
+const Header = () => {
+    return (<header className = 'container-fluid Success'>
                 <h2>Quote machine</h2>
-        </header>)
-    }
+            </header>);
 }
 
+export default Header;
